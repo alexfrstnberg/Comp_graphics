d90e8df98d63c9f957a86288ea50955ddf6574e0
alexfrstnberg <alexfrstnberg@gmail.com>
2023-04-25 19:44:53 +0300

    Add barrel

--- LR1/LR3/1.py
+++ LR1/LR3/1.py
@@ -2,64 +2,7 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
-vertices = (
-    (0, 0, 0),
-    (0, 1, 0),
-    (1, 1, 0),
-    (1, 0, 0),
-    (0, 0, 1),
-    (0, 1, 1),
-    (1, 1, 1),
-    (1, 0, 1),
-)
-
-edges = (
-    (0, 1),
-    (0, 3),
-    (0, 4),
-    (1, 2),
-    (1, 5),
-    (2, 3),
-    (2, 6),
-    (3, 7),
-    (4, 5),
-    (4, 7),
-    (5, 6),
-    (6, 7),
-)
-
-faces = (
-    (0, 1, 2, 3),
-    (3, 2, 6, 7),
-    (7, 6, 5, 4),
-    (4, 5, 1, 0),
-    (5, 6, 2, 1),
-    (7, 4, 0, 3),
-)
-
-def barrel():
-    glBegin(GL_QUADS)
-    for face in faces:
-        for vertex in face:
-            glVertex3fv(vertices[vertex])
-    glEnd()
-
-    glBegin(GL_TRIANGLES)
-    glColor3f(1, 1, 1)
-    for edge in edges:
-        # Розбиваємо ребра на 6 частин та з'єднуємо їх між собою під кутом
-        x1, y1, z1 = vertices[edge[0]]
-        x2, y2, z2 = vertices[edge[1]]
-        dx = (x2 - x1) / 6.0
-        dy = (y2 - y1) / 6.0
-        dz = (z2 - z1) / 6.0
-        for i in range(6):
-            glVertex3f(x1 + i * dx, y1 + i * dy, z1 + i * dz)
-            glVertex3f(x1 + (i + 1) * dx, y1 + (i + 1) * dy, z1 + (i + 1) * dz)
-            glVertex3f(x1 + (i + 1) * dx, y1 + (i + 1) * dy, z1 + (i + 1) * dz + 0.1)
-            glVertex3f(x1 + i * dx, y1 + i * dy, z1 + i * dz + 0.1)
-    glEnd()
+from barrel import Barrel, Hexadecagon
 
 def main():
     pygame.init()
@@ -76,8 +19,6 @@ def main():
                 quit()
 
         glRotatef(1, 3, 1, 1)
-
-
         glTranslatef(0.0,0.0,-5)
 
         while True:
@@ -88,7 +29,7 @@ def main():
 
             glRotatef(1, 3, 1, 1)
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-            barrel()
+            Barrel()
             pygame.display.flip()
             pygame.time.wait(10)
 
--- LR1/LR3/barrel.py
+++ LR1/LR3/barrel.py
@@ -4,27 +4,47 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import math
 from random import choice
+from itertools import cycle
+
 
 class Hexadecagon(list):
-    def __init__(self, radius=1, x=0, y=0, z=0):
+    def __init__(self, radius=1, x=0, y=0, z=0, color='random'):
         self.radius = radius
         self.x = x
         self.y = y
         self.z = z
-        list.__init__(self, self.base_hexadecagon_vertex_coordinates())
+        self.color = color
+        list.__init__(self, self.hexadecagon_vertex_coordinates())
 
-    def base_pentagon_vertex_coordinates(self):
-        hexadecagon=[]
+    def construct(self, color='random'):
+        glBegin(GL_LINES)
+        for vertex in self.hexadecagon_vertices():
+            glColor3fv((0, 1, 0))
+            glVertex3fv(vertex)
+        glEnd()
+
+    def hexadecagon_vertex_coordinates(self):
+        hexadecagon = []
         for n in range(16):
             x = self.x + (self.radius*math.cos(math.radians(90+n*22.5)))
             y = self.y + (self.radius*math.sin(math.radians(90+n*22.5)))
             z = self.z
             hexadecagon.append([x, y, z])
-
         return hexadecagon
 
-    def edges_pairs(self):
+    def hexadecagon_vertices(self):
+        vertices = self.hexadecagon_vertex_coordinates()
+        return self.cycled_vertices(vertices)
 
+    def cycled_vertices(self, vertices):
+        duplicated_vertices = [vertex for vertex in vertices for _ in (0, 1)]
+        cycled_vertices = []
+        cycled = cycle(duplicated_vertices)
+        next(cycled)
+        for _ in range(len(duplicated_vertices)):
+            cycled_vertices.append(next(cycled))
+
+        return cycled_vertices
 
 
 class Barrel:
@@ -32,31 +52,32 @@ class Barrel:
         self.x = x
         self.y = y
         self.z = z
-        self.radius = rsdius
-        self.height = 3*radius
-        self.edges = []
-        self.vertices = self.barrel_vertices()
+        self.radius = radius
+        self.height = 4*radius
         self.color = color
-
-    def barrel_vertices(self):
-        vertices = [] 
-        for i in range(5):
-            if i == 1 or i == 3:
-                radius *= 1.3
-            elif i == 2:
-                radius *= 1.5
-            midpoint = len(vertices) // 2
-            z = self.z + self.height // 4 * i
-
-            vertices.insert(midpoint, *Hexadecagon(self.radius, x=self.x, y=self.y, z=z))
-            edges.append
-        return vertices
+        self.construct_barrel()
 
     def construct_barrel(self):
-        glBegin(GL_LINES)
-        for edge in Barrel.all_edges():
-            for vertex in edge:
-                glColor3fv(self.choose_color(vertex))
-                glVertex3fv(self.vertices[vertex])
-        glEnd()
+        vertices = []
+        for i in range(6):
+            if i == 1 or i == 4:
+                radius = 1.3 * self.radius
+            elif i == 2 or i == 3:
+                radius = 1.4 * self.radius
+            else:
+                radius = self.radius
+            z = self.z + self.height / 6 * i
 
+            hexadecagon = Hexadecagon(radius, self.x, self.y, z)
+            hexadecagon.construct()
+
+            vertices.extend(hexadecagon)
+        self.construct_vertical_edges(vertices)
+
+    def construct_vertical_edges(self, vertices):
+        glBegin(GL_LINES)
+        for i in range(len(vertices)-16):
+            for vertex in (vertices[i], vertices[i+16]):
+                glColor3fv((1, 0, 1))
+                glVertex3fv(vertex)
+        glEnd()
